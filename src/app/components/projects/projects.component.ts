065cbb40c135a7a4341e20b548d0ceabb881e4c9
Leandro C <leandro.ca.98@gmail.com>
2024-09-24 15:30:48 -0300

    add ngfor

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,14 +1,52 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+export interface Project {
+  name: string,
+  image: string,
+  description: string;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ NgbCarouselModule ],
+  imports: [NgbCarouselModule, NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  projects: Project[] = [
+    {
+      name: 'People Generator',
+      image: 'assets/img/people-generator.png',
+      description: 'string'
+    },
+    {
+      name: 'Gifs App',
+      image: 'assets/img/gifs-app.png',
+      description: 'string'
+    },
+    {
+      name: 'Post and View',
+      image: 'assets/img/post-and-view.png',
+      description: 'string'
+    },
+    {
+      name: 'Paises App',
+      image: 'assets/img/paises-app.png',
+      description: 'string'
+    },
+    {
+      name: 'Chihiro Page',
+      image: 'assets/img/chihiro-page.png',
+      description: 'string'
+    },
+  ]
 
-images = ['assets/img/paises-app.png', 'assets/img/people-generator.png', 'assets/img/post-and-view.png' ]
+  // images = ['assets/img/paises-app.png', 'assets/img/people-generator.png', 'assets/img/post-and-view.png']
+
+  constructor() {
+    console.log(this.projects); // Verifica que el array de proyectos esté correctamente poblado
+  }
 }
