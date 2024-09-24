fdb21a7d90e39c1ff8b0e5f3e070e46c34caabce
Leandro C <leandro.ca.98@gmail.com>
2024-09-24 14:54:49 -0300

    assets and carousel

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ NgbCarouselModule ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
+images = ['assets/img/paises-app.png', 'assets/img/people-generator.png', 'assets/img/post-and-view.png' ]
 }
