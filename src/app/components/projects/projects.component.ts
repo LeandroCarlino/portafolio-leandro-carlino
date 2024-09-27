cc8eaac49b81d3edb0b8cdbdd12dee33521974ac
Leandro C <leandro.ca.98@gmail.com>
2024-09-27 12:38:00 -0300

    open pages with url

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -5,7 +5,8 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 export interface Project {
   name: string,
   image: string,
-  description: string;
+  description: string,
+  url: string,
 }
 
 @Component({
@@ -21,33 +22,42 @@ export class ProjectsComponent {
     {
       name: 'People Generator',
       image: 'assets/img/people-generator.png',
-      description: 'string'
+      description: 'string',
+      url: 'https://people-generator.vercel.app/'
     },
     {
       name: 'Gifs App',
       image: 'assets/img/gifs-app.png',
-      description: 'string'
+      description: 'string',
+      url: 'https://gifs-app-beryl.vercel.app'
     },
     {
       name: 'Post and View',
       image: 'assets/img/post-and-view.png',
-      description: 'string'
+      description: 'string',
+      url: 'https://post-and-view.vercel.app/home'
     },
     {
       name: 'Paises App',
       image: 'assets/img/paises-app.png',
-      description: 'string'
+      description: 'string',
+      url: 'https://paises-app-eight.vercel.app'
     },
     {
       name: 'Chihiro Page',
       image: 'assets/img/chihiro-page.png',
-      description: 'string'
+      description: 'string',
+      url: 'https://chihiro-page.netlify.app/'
     },
   ]
 
   // images = ['assets/img/paises-app.png', 'assets/img/people-generator.png', 'assets/img/post-and-view.png']
 
   constructor() {
-    console.log(this.projects); // Verifica que el array de proyectos esté correctamente poblado
+    console.log(this.projects);
+  }
+
+  goToPage(url: string) {
+    window.open(url, '_blank');
   }
 }
